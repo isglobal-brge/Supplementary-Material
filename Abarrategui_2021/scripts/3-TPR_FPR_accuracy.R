9e93989430525def83e8555e18844e18dc301cd5
LeireAbarrategui <abarrategui.leire@gmail.com>
2021-05-27 12:46:38 +0100

    .

--- Abarrategui_2021/scripts/3-TPR_FPR_accuracy.R
+++ Abarrategui_2021/scripts/3-TPR_FPR_accuracy.R
@@ -1,15 +1,19 @@
+# 1. Function to create TPR, FPR and accuracy list
 TPR_FPR_accuracy <- function(FPR_file_name, TPR_file_name, final_file_name){
+# 1.1. Load the data files containing TPR, FPR and accuracy list 
   load(paste0("result_files/2-TPR_accuracy/", TPR_file_name, ".rda"))
   load(paste0("result_files/2-FPR/", FPR_file_name, ".rda"))
+# 1.2. Create the table
   df <- do.call(list, lapply(seq_len(length(TPR)), function(i) {
     TPR_n <- as.data.frame(data.table::rbindlist(TPR[[i]]))
     FPR <- as.data.frame(data.table::rbindlist(FPR))
     merge(TPR_n, FPR, by = c("method", "n"), sort = FALSE)
   }))
   names(df) <- names(TPR)
-  
+# 1.3. Save results  
   save(df, file = paste0("result_files/3-TPR_FPR_accuracy/",final_file_name, ".rda"))
 }
 
+# 2. Create tables
 TPR_FPR_accuracy("GSE111629-FPR", "GSE111629-TPR", "TPR_FPR_accuracy_GSE111629")
 TPR_FPR_accuracy("GSE51032-FPR", "GSE51032-TPR", "TPR_FPR_accuracy_GSE51032")
--- Abarrategui_2021/scripts/4-graphics.R
+++ Abarrategui_2021/scripts/4-graphics.R
@@ -1,41 +1,31 @@
-#1. Load files
-load.dir <- "result_files/TPR_FPR_accuracy"
-file_name <- "GSE111629_chr_start_end"
-load(paste0(load.dir, "/", file_name,".rda"))
-#2. Set the parameters
-dataset <- "GSE111629"
-chr <- "chr5"
-start <- "67583971"
-end <- "67584381"
-
-##2.1. Only for GSE111629 dataset
-df <- df$chr5_67583971_67584381
-
-#3. Generate the graphics
-#delete <- which(is.nan(as.numeric(df$accuracy)))
-#df <- df[-delete,]
-
 library(ggplot2)
+file_name <- "TPR_FPR_accuracy_GSE111629"
 
-##3.1. graph: TPR, Accuracy and FPR graph
+load(paste0("result_files/3-TPR_FPR_accuracy/", file_name,".rda"))
 
-df <- reshape2::melt(df, id = c("method", "n"))
-dev.new(width = 1080, height = 1350, unit = "px")
-graph <- ggplot(df, aes(x = method, y = as.numeric(value), fill = factor(method))) +
-  geom_bar(stat="identity") + 
-  facet_grid(variable ~ as.numeric(n))
+names <- strsplit(names(df), "_")
+dataset_name <- sub(".*_", "", file_name)
 
-style <- graph + theme_minimal() + 
-  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.title.x = element_blank(),
-        strip.background = element_rect(color="Black", size=0.2, linetype="solid")) +
-  ggtitle(paste0(dataset,": ", chr, ":", start, " - ", end)) +
-  theme(plot.title = element_text(hjust = 0.5)) + 
-  scale_fill_manual("Method", values = c("#8F9CDA", "#DE9BB0", "#CE6D96", "#3972AA", "8E95D9", "#C5A18C")) + ylab("%") +
-  ylim(0,1) 
-style
-
-save.dir<- "result_files/Graphic/"
-ggsave(paste0(save.dir,dataset, "_", chr, "_", start, "_", end, ".png"))
+do.call(list, lapply(seq(length(df)), function(i){
+  df_region <- df[[i]]
+  df_region <- reshape2::melt(df_region, id = c("method", "n"))
+  
+  dev.new(width = 1080, height = 1350, unit = "px")
+  graph <- ggplot(df_region, aes(x = method, y = as.numeric(value), fill = factor(method))) +
+                  geom_bar(stat="identity") + 
+                  facet_grid(variable ~ as.numeric(n))
+  style <- graph + theme_minimal() + 
+    theme(axis.text.x = element_blank(), 
+          axis.ticks.x = element_blank(), 
+          axis.title.x = element_blank(),
+          strip.background = element_rect(color="Black", size=0.2, linetype="solid")) +
+    ggtitle(paste0(dataset_name,": ", names[[i]][1], ":", names[[i]][2], " - ", names[[i]][3])) +
+    theme(plot.title = element_text(hjust = 0.5)) + 
+    scale_fill_manual("Method", values = c("#8F9CDA", "#DE9BB0", "#CE6D96", "#3972AA", "8E95D9", "#C5A18C")) + ylab("%") +
+    ylim(0,1) 
+  style
+  ggsave(paste0( "result_files/3-TPR_FPR_accuracy/", dataset_name, "_", names[[i]][1], "_", names[[i]][2], "_", names[[i]][3], ".png"))
+  }))
 
 
 
